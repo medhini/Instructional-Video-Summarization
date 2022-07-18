7bffb1cc5cebeedfc928b89de8a08bf99a3502f9
Medhini <medhini95@gmail.com>
2022-07-18 23:07:29 +0000

    Feature extraction

---
+++ feature_extraction/data.py
@@ -0,0 +1,87 @@
+import argparse
+import os
+import math
+import torch
+import torch.nn.functional as F
+import numpy as np
+import torchvision.transforms as transforms
+import torchvision.io as io
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+import imageio
+import copy
+
+from types import SimpleNamespace
+
+
+class MIL_NCE_Extract_Feats(Dataset):
+    def __init__(self, args):
+        self.v_path = args.vdata
+        self.asr_path = args.asr_path
+        self.window = args.window
+        self.dataset = args.dataset
+
+        self.asr_files = sorted(os.listdir(self.asr_path))
+
+        if self.dataset == "wikihowto":
+            all_vid_files = sorted(os.listdir(self.v_path))
+            self.vid_files = []
+
+            for file in all_vid_files:
+                if file.split(".")[0] + ".en.txt" in self.asr_files:
+                    self.vid_files.append(file)
+
+        elif self.dataset == "crosstask":
+            self.vid_files = sorted(os.listdir(self.v_path))
+            # skip done, errors
+            done = os.listdir(
+                "/home/medhini/video_summarization/task_video_sum/datasets/pseudoGT_milnce_feats_8fps/video/embedding".format(
+                    self.dataset
+                )
+            )
+            for f in done:
+                if f.split(".")[0] + ".mp4" in self.vid_files:
+                    self.vid_files.remove(f.split(".")[0] + ".mp4")
+                if f.split(".")[0] + ".mkv" in self.vid_files:
+                    self.vid_files.remove(f.split(".")[0] + ".mkv")
+                if f.split(".")[0] + ".webm" in self.vid_files:
+                    self.vid_files.remove(f.split(".")[0] + ".webm")
+
+        self.transform = transforms.Resize((224, 224))
+
+    def __len__(self):
+        return len(self.vid_files)
+
+    def __getitem__(self, idx):
+        video_path = os.path.join(self.v_path, self.vid_files[idx])
+        print("Starting video: ", self.vid_files[idx])
+
+        assert os.path.exists(video_path), "No video found at {}".format(
+            self.vid_files[idx]
+        )
+
+        video, _, meta = io.read_video(video_path, pts_unit="sec")
+
+        # window_len = int(self.window * meta["video_fps"])
+        window_len = 32
+
+        # Pad video with extra frames to ensure its divisible by window_len
+        extra_frames = window_len - (len(video) % window_len)
+        video = torch.cat((video, video[-extra_frames:]), dim=0)
+
+        n_segs = int(video.shape[0] / window_len)
+
+        print("Number of video segments: ", n_segs)
+
+        video = video.view(n_segs, 32, video.shape[1], video.shape[2], 3)
+        video = video.permute(0, 1, 4, 2, 3)
+
+        # Transform video segments
+        video_segs = []
+        for seg in video:
+            # Resize and normalize to [0,1]
+            video_segs.append(self.transform(seg) / 255.0)
+        video_segs = torch.stack(video_segs)
+        video_segs = video_segs.view(n_segs, 32, 3, 224, 224)
+
+        return video_segs, self.vid_files[idx].split(".")[0]
---
+++ feature_extraction/extract_text_video_feats.py
@@ -0,0 +1,178 @@
+import os
+import sys
+import argparse
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import math
+
+from s3dg import S3D
+from data import MIL_NCE_Extract_Feats
+
+parser = argparse.ArgumentParser(description="PyTorch Video Textures")
+parser.add_argument(
+    "--window", "-w", default=1, type=int, help="Size of temporal window in seconds"
+)
+parser.add_argument(
+    "--batch_size",
+    "-bs",
+    default=1,
+    type=int,
+    metavar="N",
+    help="mini-batch size (default: 32)",
+)
+parser.add_argument(
+    "--vdata",
+    "-vdata",
+    default="/home/medhini/video_summarization/task_video_sum/datasets/pseudoGT_videos",
+    type=str,
+    help="Path to video dataset",
+)
+
+parser.add_argument(
+    "--asr_path",
+    "-asr_dir",
+    default="/home/medhini/video_summarization/task_video_sum/datasets/pseudoGT_asr",
+    type=str,
+    help="Path to asr",
+)
+
+parser.add_argument(
+    "-rf",
+    "--results_folder",
+    default="/home/medhini/video_summarization/task_video_sum/datasets/pseudoGT_milnce_feats_8fps",
+    type=str,
+    help="folder for result videos",
+)
+
+parser.add_argument(
+    "--workers",
+    "-j",
+    default=0,
+    type=int,
+    metavar="N",
+    help="number of data loading workers (default: 8)",
+)
+parser.add_argument(
+    "--dataset",
+    "-dataset",
+    default="crosstask",
+    type=str,
+    help="Dataset name",
+)
+
+
+def to_cuda(item):
+    if isinstance(item[0], list):
+        return [[x.cuda() for x in y] for y in item]
+    elif isinstance(item, list):
+        return [x.cuda() for x in item]
+    return item.cuda()
+
+
+def main(args):
+    # create dataset
+    dataset = MIL_NCE_Extract_Feats(args)
+
+    # create training loader
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.workers,
+        drop_last=False,
+    )
+
+    # Instantiate the model
+    net = S3D("s3d_dict.npy", 512)
+
+    # Load the model weights
+    net.load_state_dict(torch.load("s3d_howto100m.pth"))
+    net = torch.nn.DataParallel(net).cuda()
+    cudnn.benchmark = True
+
+    # Evaluation mode
+    net = net.eval()
+
+    # Video input should be of size Batch x 3 x T x H x W and normalized to [0, 1]
+    # video = torch.rand(2, 3, 32, 224, 224)
+
+    # Create results folders
+    os.makedirs(os.path.join(args.results_folder, "video", "embedding"), exist_ok=True)
+    os.makedirs(os.path.join(args.results_folder, "video", "mixed_5c"), exist_ok=True)
+    os.makedirs(os.path.join(args.results_folder, "asr"), exist_ok=True)
+
+    with torch.no_grad():
+        for itr, batch_data in enumerate(dataloader):
+            (video_segs, video_name) = batch_data
+
+            # Compute MIL-NCE feats for video segments
+
+            # [1, B, T, C, H, W] -> [B, C, T, H, W]
+            video_segs = video_segs.squeeze(0).permute(0, 2, 1, 3, 4)
+            batch_size = video_segs.shape[0]
+            num_gpus = torch.cuda.device_count()
+            gpu_bs = 30
+            mini_batch_size = gpu_bs * num_gpus
+
+            vid_feats = []
+            vid_mixed_5c = []
+
+            for j in range(math.ceil(batch_size / mini_batch_size)):
+                this_batch = video_segs[j * mini_batch_size : (j + 1) * mini_batch_size]
+                this_batch = to_cuda(this_batch)
+                vid_feats.extend(net(this_batch)["video_embedding"].detach().cpu())
+                vid_mixed_5c.extend(net(this_batch)["mixed_5c"].detach().cpu())
+            vid_feats = torch.stack(vid_feats)
+            vid_mixed_5c = torch.stack(vid_mixed_5c)
+            print("Size of vid feats: ", vid_feats.shape)
+
+            # Store MIL-NCE feats for each video
+            torch.save(
+                vid_feats,
+                os.path.join(
+                    args.results_folder, "video", "embedding", video_name[0] + ".pth"
+                ),
+            )
+            torch.save(
+                vid_mixed_5c,
+                os.path.join(
+                    args.results_folder, "video", "mixed_5c", video_name[0] + ".pth"
+                ),
+            )
+
+            print("Saved feats for: ", video_name[0])
+
+    # Instantiate the model
+    net = S3D("s3d_dict.npy", 512)
+
+    # Load the model weights
+    net.load_state_dict(torch.load("s3d_howto100m.pth"))
+
+    # Evaluation mode
+    net = net.eval()
+
+    asr_files = os.listdir(args.asr_path)
+    with torch.no_grad():
+        for asr_file in asr_files:
+            video_name = asr_file.split(".")[0]
+            asr_file = os.path.join(args.asr_path, asr_file)
+            with open(asr_file, "r") as f:
+                asr_sentences = f.read().splitlines()
+
+                # Text inference
+                asr_feats = net.text_module(asr_sentences)["text_embedding"].detach()
+
+                print("Size of asr feats: ", asr_feats.shape)
+
+                torch.save(
+                    asr_feats,
+                    os.path.join(args.results_folder, "asr", video_name + ".pth"),
+                )
+                print("Saved asr feats for: ", video_name)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    assert os.path.exists(args.vdata), "No videos found at {}".format(args.vdata)
+    main(args)
